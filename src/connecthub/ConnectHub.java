9770a6eef851b9aa3e645b2e82ee1b7d01fe8b39
minaemil10 <minaemile3@gmail.com>
2024-12-03 00:12:22 +0200

    initialized repo

---
+++ src/connecthub/ConnectHub.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package connecthub;
+
+/**
+ *
+ * @author Mnw Emile
+ */
+public class ConnectHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
