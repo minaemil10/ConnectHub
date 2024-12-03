2ae8cfd5536d61038d0550991c333692c6156c2b
minaemil10 <minaemile3@gmail.com>
2024-12-03 17:14:09 +0200

    Create Story.java

---
+++ src/Backend/Story.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+/**
+ *
+ * @author Mnw Emile
+ */
+public class Story extends Content{
+
+    public Story(String photo, String contentID, String authorID, String text) {
+        super(photo, contentID, authorID, text);
+    }
+    
+}
