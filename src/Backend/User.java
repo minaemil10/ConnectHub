d95b684fb429af4cceace463ae9432ef4469275f
Carl Sherif Kheir Eskander <145164885+carlsherif@users.noreply.github.com>
2024-12-03 13:29:09 +0200

    Class user added

---
+++ src/Backend/User.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author carls
+ */
+public class User {
+    private String userId;
+    private String email;
+    private String userName;
+    private String password;
+    private LocalDate DateOfBirth;
+    private Boolean status;
+    private String profilePhoto;
+    private String coverPhoto;
+    private String bio;
+    private ArrayList<String> friends;
+    private ArrayList<String> myPosts;
+    
+}
