12f67f6d9c89be80f5ac5a17163264f20739159d
Carl Sherif Kheir Eskander <145164885+carlsherif@users.noreply.github.com>
2024-12-03 13:31:07 +0200

    Constructor and getters and setters

--- src/Backend/User.java
+++ src/Backend/User.java
@@ -23,5 +23,101 @@ public class User {
     private String bio;
     private ArrayList<String> friends;
     private ArrayList<String> myPosts;
+
+    public User(String userId, String email, String userName, String password, LocalDate DateOfBirth) {
+        this.userId = userId;
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+        this.DateOfBirth = DateOfBirth;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return DateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate DateOfBirth) {
+        this.DateOfBirth = DateOfBirth;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public String getCoverPhoto() {
+        return coverPhoto;
+    }
+
+    public void setCoverPhoto(String coverPhoto) {
+        this.coverPhoto = coverPhoto;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public ArrayList<String> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(ArrayList<String> friends) {
+        this.friends = friends;
+    }
+
+    public ArrayList<String> getMyPosts() {
+        return myPosts;
+    }
+
+    public void setMyPosts(ArrayList<String> myPosts) {
+        this.myPosts = myPosts;
+    }
     
 }
