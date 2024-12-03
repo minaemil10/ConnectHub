82e36b3d24ddf4d18c8091a70c737505a4d0a4db
danielWadie224 <danielwadie2004@gmail.com>
2024-12-03 15:21:10 +0200

    login & Logout done

---
+++ src/Backend/Login.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+/**
+ *
+ * @author User
+ */
+public class Login {
+    private User user;
+
+    public Login(User user) {
+        this.user = user;
+    }
+    public User accessUser (String Email, String Password){
+        return user;
+    }
+}
