e9008362c84f2d544d17d358a2b82fc9c035a3ac
minaemil10 <minaemile3@gmail.com>
2024-12-04 01:04:26 +0200

    FileWriter of Users
    Cretaed method to write all users to the .json file of the users

---
+++ src/Backend/DataBase/FilePaths.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Backend.DataBase;
+
+/**
+ *
+ * @author Mnw Emile
+ */
+public interface FilePaths {
+    String userDataBase = "C:\\Users\\Mnw Emile\\Desktop\\Prog_Labs\\prog2_lab8_socialApp\\ConnectHub\\src\\Backend\\DataBase\\users.json";
+    
+}
--- src/Backend/DataBase/JsonFileWriter.java
+++ src/Backend/DataBase/JsonFileWriter.java
@@ -18,32 +18,70 @@ import java.util.ArrayList;
  *
  * @author Mnw Emile
  */
-public class JsonFileWriter {
+public class JsonFileWriter implements FilePaths{
 
-    public static void main(String[] args) {
-        Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()) .setPrettyPrinting() .create();
-        LocalDate date = LocalDate.of(2004, Month.OCTOBER, 26);
-        User user = new User("U1", "mina@gmail.com", "Mina Emile", "123456", date);
-        user.setBio("I am an Engineer");
-        user.setCoverPhoto("src\\Mina.png");
-        user.setStatus(Boolean.TRUE);
-        ArrayList<Relationship> friends = new ArrayList<>();
-        Relation friend = new Relation();
-        friend.setFriend();
-        friends.add(0, new Relationship("U2", friend));
-        Relation friend2 = new Relation();
-        friend2.setBlock();
-        friends.add(0, new Relationship("U3", friend2));
-        user.setFriends(friends);
+    private String filePath;
 
-        String json = gson.toJson(user);
-        
-        try (FileWriter fileWriter = new FileWriter("C:\\Users\\Mnw Emile\\Desktop\\Prog_Labs\\prog2_lab8_socialApp\\ConnectHub\\src\\Backend\\DataBase\\users.json",false)) {
+    public JsonFileWriter(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void writeAllUsers(ArrayList<User> users) {
+        Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).setPrettyPrinting().create();
+
+        //check for rlations set to "cancel" and remove them
+        for (User user : users) {
+            ArrayList<Relationship> relations = user.getFriends();
+            if (relations != null) {
+                for (Relationship rel : relations) {
+                    if (rel.getRelation().equals("Cancel"))
+                    {
+                        System.out.println(rel.getRelation());
+                        user.deleteRelation(rel);
+                    }
+                }
+            }
+        }
+        //write user to file
+        String json = gson.toJson(users);
+        try (FileWriter fileWriter = new FileWriter(filePath, false)) {
             fileWriter.write(json);
-            System.out.println("Successfully wrote JSON object to file.");
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        LocalDate date1 = LocalDate.of(2004, Month.OCTOBER, 26);
+        User user1 = new User("U1", "mina@gmail.com", "Mina Emile", "123456", date1);
+        user1.setBio("I am an Engineer");
+        user1.setCoverPhoto("src\\Mina.png");
+        user1.setStatus(Boolean.TRUE);
+        ArrayList<Relationship> relationships = new ArrayList<>();
+        user1.setFriends(relationships);
+        
+        Relation rel2 = new Relation();
+        rel2.setBlock();
+        Relationship relation2 = new Relationship("U3", rel2);
+        user1.addRelation(relation2);
+        
+        Relation rel1 = new Relation();
+        rel1.setFriend();
+        Relationship relation1 = new Relationship("U2", rel1);
+        user1.addRelation(relation1);
+      
+        
+        
+        LocalDate date2 = LocalDate.of(2001, Month.JANUARY, 15);
+        User user2 = new User("U2", "john@example.com", "John Doe", "password", date2);
+        user2.setBio("I am a Developer");
+        user2.setCoverPhoto("src\\John.png");
+        user2.setStatus(Boolean.FALSE);
+        ArrayList<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        JsonFileWriter writer = new JsonFileWriter(userDataBase);
+        writer.writeAllUsers(users);
     }
 
 }
