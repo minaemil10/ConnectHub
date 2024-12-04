48cbcd7c63f6c1695a30dc7df755461efd4190aa
minaemil10 <minaemile3@gmail.com>
2024-12-04 22:05:50 +0200

    readContent feature added
    can readAllcontent, posts ONLY or stories ONLY

--- src/Backend/DataBase/JsonFileReader.java
+++ src/Backend/DataBase/JsonFileReader.java
@@ -4,7 +4,10 @@
  */
 package Backend.DataBase;
 
+import Backend.Content;
 import Backend.Friend_Management.friendRequest;
+import Backend.Post;
+import Backend.Story;
 import Backend.User;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -33,7 +36,6 @@ public class JsonFileReader implements FilePaths {
             Type userListType = new TypeToken<ArrayList<User>>() {
             }.getType();
             users = gson.fromJson(fileReader, userListType);
-            System.out.println(users);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -47,11 +49,60 @@ public class JsonFileReader implements FilePaths {
             Type reqListType = new TypeToken<ArrayList<friendRequest>>() {
             }.getType();
             req = gson.fromJson(fileReader, reqListType);
-            System.out.println(req);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
         return req;
+    }
+
+
+    public ArrayList<Content> readAllContent(){
+        Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).setPrettyPrinting().create();
+        ArrayList<Content> Content = new ArrayList<>();
+        try (FileReader fileReader = new FileReader(filePath)) {
+            Type contentListType = new TypeToken<ArrayList<Content>>() {
+            }.getType();
+            Content = gson.fromJson(fileReader, contentListType);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Content;
+    }
+
+    public ArrayList<Story> readAllStories(){
+        ArrayList<Story> stories = new ArrayList<>();
+        JsonFileReader reader = new JsonFileReader(filePath);
+        ArrayList<Content> contents =  reader.readAllContent();
+        for(Content con : contents){
+            if(con.getContentID().startsWith("S")){
+                stories.add(contentToStory(con));
+            }
+        }
+        return stories;
+    }
+
+    public ArrayList<Post> readAllPosts(){
+        ArrayList<Post> posts = new ArrayList<>();
+        JsonFileReader reader = new JsonFileReader(filePath);
+        ArrayList<Content> contents =  reader.readAllContent();
+        for(Content con : contents){
+            if(con.getContentID().startsWith("P")){
+                posts.add(contentToPost(con));
+            }
+        }
+        return posts;
+    }
+
+
+    public static Story contentToStory(Content con){
+        return new Story(con.getPhoto(),con.getContentID(),con.getAuthorID(),con.getText());
+    }
+    public static Post contentToPost(Content con){
+        Post post =  new Post(con.getContentID(),con.getAuthorID(),con.getText());
+        post.setPhoto(con.getPhoto());
+        return post;
     }
 
     public static void main(String[] args) {
@@ -59,5 +110,7 @@ public class JsonFileReader implements FilePaths {
         reader.readAllUsers();
         JsonFileReader reader2 = new JsonFileReader(requestsDataBase);
         reader2.readAllRequests();
+        JsonFileReader reader3 = new JsonFileReader(contentDataBase);
+        reader3.readAllPosts();
     }
 }
