e49f49a3dd0545181df43d3763679061ebbe98e5
Carl Sherif Kheir Eskander <145164885+carlsherif@users.noreply.github.com>
2024-12-10 16:28:27 +0200

    Update Notification.java

--- src/Backend/Notification.java
+++ src/Backend/Notification.java
@@ -3,11 +3,45 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Backend;
-
+import Backend.Friend_Management.friendRequest;
+import java.util.ArrayList;
 /**
  *
  * @author carls
  */
 public class Notification {
+    private ArrayList<friendRequest> friendRequest;
+    private ArrayList<Group> groupNotification;
+    private ArrayList<Post> groupPosts;
+
+    public Notification() {
+        this.friendRequest = new ArrayList<>();
+        this.groupNotification = new ArrayList<>();
+        this.groupPosts = new ArrayList<>();
+    }
+
+    public ArrayList<friendRequest> getFriendRequest() {
+        return friendRequest;
+    }
+
+    public void setFriendRequest(ArrayList<friendRequest> friendRequest) {
+        this.friendRequest = friendRequest;
+    }
+
+    public ArrayList<Group> getGroupNotification() {
+        return groupNotification;
+    }
+
+    public void setGroupNotification(ArrayList<Group> groupNotification) {
+        this.groupNotification = groupNotification;
+    }
+
+    public ArrayList<Post> getGroupPosts() {
+        return groupPosts;
+    }
+
+    public void setGroupPosts(ArrayList<Post> groupPosts) {
+        this.groupPosts = groupPosts;
+    }
     
 }
