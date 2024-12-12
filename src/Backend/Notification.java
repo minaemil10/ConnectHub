0c929397e1c9ee764f037f1941b263e844ef1a72
danielWadie224 <danielwadie2004@gmail.com>
2024-12-12 17:43:18 +0200

    app manager and notification updated

--- src/Backend/Notification.java
+++ src/Backend/Notification.java
@@ -12,21 +12,92 @@ import Backend.DataBase.DataBaseOBJ;
  */
 public class Notification implements DataBaseOBJ{
     private String name;
+    private String type;
     private String userId;
     private String profilePhoto;
     private static int num = 0;
     private String Id;
-    public Notification(String name, String userId, String profilePhoto) {
+    private String user;
+    private Content post;
+    private String status; 
+    public Notification(String type, String name, String userId, String profilePhoto) {
         this.name = name;
         this.userId = userId;
         this.profilePhoto = profilePhoto;
         num++;
         Id = "n"+num; 
-        
     }
-    
-    
-    
+
+    public Notification(String type, String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Notification(String type, String user, Content post) {
+        this.user = user;
+        this.post = post;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public static void setNum(int num) {
+        Notification.num = num;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public static int getNum() {
+        return num;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setPost(Content post) {
+        this.post = post;
+    }
+
+    public Content getPost() {
+        return post;
+    }
+
     public  String getId() {
         return Id;
     }
